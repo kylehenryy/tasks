64485cd755c5d15a11da5a7cf50f25e591a4c039
Kyle <kylech@udel.edu>
2026-02-12 19:03:00 -0500

    Completed HTML and CSS task

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h1>UD CISC275 with React Hooks and TypeScript</h1>
             </header>
+            <img src="pic_trulli.jpg" alt="Trulli" />
+            <ul>
+                <li>Chocolate Ice Cream</li>
+                <li>Vanilla Ice Cream</li>
+                <li>Strawberry Ice Cream</li>
+            </ul>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
             <p>Kyle Henry</p>
-            <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
