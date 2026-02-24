23fc875d4e23c9a07deceb79ff28d6e9fc3d50cf
Kyle <kylech@udel.edu>
2026-02-24 00:30:07 -0500

    stringsToIntergers array function complete

--- src/arrays.ts
+++ src/arrays.ts
@@ -26,7 +26,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map((s: string): number => {
+        const parseString = parseInt(s, 10);
+        return Number.isNaN(parseString) ? 0 : parseString;
+    });
 }
 
 /**
